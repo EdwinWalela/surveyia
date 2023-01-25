efedfebbd7774460db1a410226a5e4f05ede7491
edwinwalela <edwinwalela@gmail.com>
2023-01-26 00:23:08 +0300

    modularize dashboard components

--- src/pages/Dashboard/surveyListTable.tsx
+++ src/pages/Dashboard/surveyListTable.tsx
@@ -2,22 +2,25 @@ import SurveyListTableItem from './tableItem';
 
 const SurveyListTable = () => {
 	return (
-		<table className="md:w-full md:mx-auto my-4 mb-16  text-left shadow-md ">
-			<thead>
-				<tr className="bg-slate-700 text-gray-300">
-					<th className="w-3/4 px-6 py-2 text-xs">TITLE</th>
-					<th className="w-1/4 px-6 py-2 text-xs text-center">ACTIONS</th>
-				</tr>
-			</thead>
-			<tbody>
-				<SurveyListTableItem />
-				<SurveyListTableItem />
-				<SurveyListTableItem />
-				<SurveyListTableItem />
-				<SurveyListTableItem />
-				<SurveyListTableItem />
-			</tbody>
-		</table>
+		<>
+			<h1 className="text-xl font-medium">Your Surveys</h1>
+			<table className="md:w-full md:mx-auto my-4 mb-16  text-left shadow-md ">
+				<thead>
+					<tr className="bg-slate-700 text-gray-300">
+						<th className="w-3/4 px-6 py-2 text-xs">TITLE</th>
+						<th className="w-1/4 px-6 py-2 text-xs text-center">ACTIONS</th>
+					</tr>
+				</thead>
+				<tbody>
+					<SurveyListTableItem />
+					<SurveyListTableItem />
+					<SurveyListTableItem />
+					<SurveyListTableItem />
+					<SurveyListTableItem />
+					<SurveyListTableItem />
+				</tbody>
+			</table>
+		</>
 	);
 };
 
