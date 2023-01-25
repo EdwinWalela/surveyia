c690f56b440ddb68c17915f98263229ec660f4c6
edwinwalela <edwinwalela@gmail.com>
2023-01-26 00:29:57 +0300

    create survey button

--- src/pages/Dashboard/surveyListTable.tsx
+++ src/pages/Dashboard/surveyListTable.tsx
@@ -1,14 +1,20 @@
+import { Link } from 'react-router-dom';
 import SurveyListTableItem from './tableItem';
 
 const SurveyListTable = () => {
 	return (
 		<>
-			<h1 className="text-xl font-medium">Your Surveys</h1>
+			<div className="md:flow-root">
+				<h1 className="text-xl font-medium md:float-left pt-2">Your Surveys</h1>
+				<button className="md:float-right bg-black text-white px-4 py-2 rounded-lg border hover:border-black hover:bg-white hover:text-black active:scale-95 transition-all">
+					<Link to="#">Create A Survey</Link>
+				</button>
+			</div>
 			<table className="md:w-full md:mx-auto my-4 mb-16  text-left shadow-md ">
 				<thead>
-					<tr className="bg-slate-700 text-gray-300">
-						<th className="w-3/4 px-6 py-2 text-xs">TITLE</th>
-						<th className="w-1/4 px-6 py-2 text-xs text-center">ACTIONS</th>
+					<tr className="bg-black text-gray-300">
+						<th className="w-3/4 px-6 py-3 text-xs">TITLE</th>
+						<th className="w-1/4 px-6 py-3 text-xs text-center">ACTIONS</th>
 					</tr>
 				</thead>
 				<tbody>
