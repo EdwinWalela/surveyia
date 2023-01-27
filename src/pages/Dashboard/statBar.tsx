b1648b704cf83528cc24e4c2159b0f3a4cedd74d
edwinwalela <edwinwalela@gmail.com>
2023-01-27 20:28:27 +0300

    update dashboard stat bar colors

--- src/pages/Dashboard/statBar.tsx
+++ src/pages/Dashboard/statBar.tsx
@@ -1,15 +1,15 @@
 const StatBar = () => {
 	return (
 		<div className="md:flow-root mb-8 hidden">
-			<div className="md:w-1/4 w-full bg-black text-white md:float-left p-10 rounded-lg shadow-lg text-center mx-10 cursor-pointer hover:bg-white hover:text-black border hover:border-black transition-all ease-in-out">
+			<div className="md:w-1/4 w-full bg-blue-500 text-white md:float-left p-10 rounded-lg shadow-lg text-center mx-10 cursor-pointer hover:bg-white hover:text-blue-500 border hover:border-blue-500 transition-all ease-in-out">
 				<h1 className="text-4xl font-medium">KES 1100</h1>
 				<h1 className="text-lg  mt-3  font-medium">Wallet Balance</h1>
 			</div>
-			<div className="md:w-1/4 w-full bg-black text-white md:float-left p-10 rounded-lg shadow-lg text-center mx-10 cursor-pointer hover:bg-white hover:text-black border hover:border-black transition-all ease-in-out">
+			<div className="md:w-1/4 w-full bg-slate-700 text-white md:float-left p-10 rounded-lg shadow-lg text-center mx-10 cursor-pointer hover:bg-white hover:text-slate-700 border hover:border-slate-700 transition-all ease-in-out">
 				<h1 className="text-4xl  font-medium"> 10</h1>
 				<h1 className="text-lg mt-3 font-medium">Total Questionnaires</h1>
 			</div>
-			<div className="md:w-1/4 w-full bg-black text-white md:float-left p-10 rounded-lg shadow-lg text-center mx-10 cursor-pointer hover:bg-white hover:text-black border hover:border-black transition-all ease-in-out">
+			<div className="md:w-1/4 w-full bg-indigo-700 text-white md:float-left p-10 rounded-lg shadow-lg text-center mx-10 cursor-pointer hover:bg-white hover:text-indigo-700 border hover:border-indigo-700 transition-all ease-in-out">
 				<h1 className="text-4xl  font-medium">34</h1>
 				<h1 className="text-lg mt-3 font-medium">Total Respondents</h1>
 			</div>
